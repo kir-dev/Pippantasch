926ba00e6fd35b78d4148e13a3606113b8c2f3fb
fenyopeti <petyu8@msn.com>
2018-02-09 17:02:28 +0100

    added routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+/* GET sell page. */
+router.get('/eladas', function(req, res, next) {
+  res.render('sell-item');
+});
+
 
 module.exports = router;
